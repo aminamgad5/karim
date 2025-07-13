7f7059b4a23ac2b7504a66fdec111389cb1b20cc
aminamgad5 <aminamgaf@gmail.com>
2025-07-13 16:00:19 +0300

    Updated popup.js

--- popup.js
+++ popup.js
@@ -492,6 +492,7 @@ class ETAInvoiceExporter {
   }
   
   formatInteractiveWorksheet(ws, headers, dataLength) {
+  }
   formatWorksheet(ws, headers, dataLength) {
     // Set column widths
     const colWidths = [
